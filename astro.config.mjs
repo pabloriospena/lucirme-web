e4083690bcc34e09aff31cfdccb17fed7dd9f4d1
pabloriospena <pabloriospena@gmail.com>
2026-05-07 19:59:10 -0500

    Fix: Config híbrida para formularios y estática

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel'; // ⬅️ VUELVE A IMPORTAR ESTO
 
 export default defineConfig({
-  output: 'static',
-    trailingSlash: 'ignore', // ← Esto evita conflictos de rutas
+  output: 'hybrid', // ⬅️ CAMBIA 'static' POR 'hybrid'
+  adapter: vercel(), // ⬅️ VUELVE A AGREGAR EL ADAPTER
+  
   vite: {
     plugins: [tailwindcss()]
   }
